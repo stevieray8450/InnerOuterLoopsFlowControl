3df378e9468ddc54e8183ae008cfc879c4d03368
Stephen Boniface <stevieray8450@gmail.com>
2017-06-01 21:14:48 -0500

    first commit

---
+++ InnerOuterLoopsFlowControl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InnerOuterLoopsFlowControl
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userInput = Console.ReadLine();
+            int userNum = Int32.Parse(userInput);
+
+            for (int i = 1; i <= userNum; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
